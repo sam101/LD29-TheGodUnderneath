b3d9a281b67064f44b0a870cbae442c0a7947614
Samuel Lepetit <lepetit.samuel@gmail.com>
2014-04-27 18:57:17 +0200

    Update introduction

--- static/js/introduction.js
+++ static/js/introduction.js
@@ -1,15 +1,18 @@
 
-function introductionListener(event) {
-	if (event.keyCode == KEY_ENTER) {
-		$('body').off('keydown', introductionListener);
-		$('#introduction').slideUp(INTRODUCTION_DELAY);
-		setTimeout(function() {
-			$('#gamePlay').slideDown(INTRODUCTION_DELAY);
+$(document).ready(function() {
+	function introductionListener(event) {
+		if (event.keyCode == KEY_ENTER) {
+			$('body').off('keydown', introductionListener);
+			$('#introduction').slideUp(INTRODUCTION_DELAY);
 			setTimeout(function() {
-				initGame();
+				$('#gamePlay').slideDown(INTRODUCTION_DELAY);
+				setTimeout(function() {
+					initGame();
+				}, INTRODUCTION_DELAY);
 			}, INTRODUCTION_DELAY);
-		}, INTRODUCTION_DELAY);
+		}
 	}
-}
 
-$('body').on('keydown', introductionListener);
+	$('body').on('keydown', introductionListener);	
+});
+
